73bc67e5bb40d24c506d536836d55ed6ecca729f
DylanStruik <dylan.struik@outlook.com>
2017-12-03 16:43:44 +0100

    Cubic Bezier update

--- Animaties/animate.js
+++ Animaties/animate.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('animeerKnop');
 const vlakje = document.querySelectorAll('.vlakje');
 vlakje[0].style.backgroundColor = 'red';
-vlakje[1].style.backgroundColor = 'lightgrey';
+vlakje[1].style.backgroundColor = 'white';
 vlakje[2].style.backgroundColor = 'blue';
 const schuif = () => {
     for (let i = 0; i < vlakje.length; i++) {
